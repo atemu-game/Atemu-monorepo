1644cddd668eff9ead1c028741968ae1188a2911
BrianSin <baomap.1203@gmail.com>
2024-08-15 14:15:53 +0700

    fix get history winning

--- packages/api-service/src/fuel/dto/winningHistory.dto.ts
+++ packages/api-service/src/fuel/dto/winningHistory.dto.ts
@@ -1,13 +1,15 @@
 import { BaseQueryParams } from '@app/shared/types';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsHexadecimal } from 'class-validator';
+import { IsBoolean, IsHexadecimal, IsOptional } from 'class-validator';
 
 export class QueryWinningHistoryDto extends BaseQueryParams {
   @ApiProperty()
   @IsHexadecimal()
-  user: string;
+  @IsOptional()
+  user?: string;
 
   @ApiProperty()
   @IsBoolean()
+  @IsOptional()
   isClaimed?: boolean;
 }
